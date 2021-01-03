c3ff476dea5fbd38a66f66f0b74f2721baa29a1d
Chaoyue Zhao <czhao@venacorp.com>
2021-01-03 17:11:02 -0500

    ⚡️ some updates

--- .hyper.js
+++ .hyper.js
@@ -12,7 +12,8 @@ module.exports = {
 		fontSize: 15,
 
 		// font family with optional fallbacks
-		fontFamily: '"Fira Code", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+		fontFamily:
+			'"Terminess Powerline", "Anonymice Powerline", "Roboto Mono for Powerline", "Meslo LG S for Powerline", "DejaVu Sans Mono", "Lucida Console", monospace',
 
 		// default font weight: 'normal' or 'bold'
 		fontWeight: "normal",
